e11010e36cfc7bacc547527a59404cb414dc6faf
iamyajat <68477362+iamyajat@users.noreply.github.com>
2022-06-01 02:12:54 +0530

    feat: spam classifier

--- app/src/main/java/com/dscvit/notix/NotificationListener.kt
+++ app/src/main/java/com/dscvit/notix/NotificationListener.kt
@@ -3,6 +3,7 @@ package com.dscvit.notix
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
+import com.dscvit.notix.util.SpamClassifier.spamClassifyMessage
 
 class NotificationListener : NotificationListenerService() {
 
@@ -11,6 +12,17 @@ class NotificationListener : NotificationListenerService() {
     }
 
     override fun onNotificationPosted(newNotification: StatusBarNotification) {
+        // Building the input message string
+        var message = ""
+        newNotification.notification.extras.apply {
+            message += getString("android.title") + " "
+            message += getString("android.text")
+        }
+
+        // Classify the notification
+        val isSpam: Boolean = spamClassifyMessage(this, message)
+
+        // Logging the notification
         Log.i(
             TAG,
             "------------OnNotificationPosted" +
@@ -21,7 +33,8 @@ class NotificationListener : NotificationListenerService() {
                     "\n" + "text: " + newNotification.notification.extras.getString("android.text") +
                     "\n" + "tickerText: " + newNotification.notification.tickerText +
                     "\n" + "postedTime: " + newNotification.postTime +
-                    "\n" + "package name: " + newNotification.packageName
+                    "\n" + "packageName: " + newNotification.packageName +
+                    "\n" + "isSpam: " + isSpam.toString()
 
             /*"-------- onNotificationPosted(): " + "ID :" + newNotification.id +
                     "\n" + newNotification.notification.extras.getString("android.title") +
---
+++ app/src/main/java/com/dscvit/notix/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.dscvit.notix.util
+
+object Constants {
+    const val SPAM_POINT = 0.8
+}
