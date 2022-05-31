71cb46869ba8843eea45d31f20a4ca06028b28df
iamyajat <68477362+iamyajat@users.noreply.github.com>
2022-06-01 02:12:54 +0530

    feat: spam classifier

--- app/src/main/java/com/dscvit/notix/MainActivity.kt
+++ app/src/main/java/com/dscvit/notix/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.dscvit.notix
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+import com.dscvit.notix.util.UtilFunctions.isNotificationServiceRunning
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getNotificationAccess()
+    }
+
+    private fun getNotificationAccess() {
+        val isNotificationServiceRunning: Boolean = isNotificationServiceRunning(this)
+        if (!isNotificationServiceRunning) {
+            startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
+        }
     }
 }
